58cc2c5d96684b856f968858c7d04e9a77820571
c-bec-k <cruthenbeck@gmail.com>
2022-10-28 12:32:05 -0500

    added SqlDbContext

---
+++ BudgetApp.SqlDbServices/BudgetAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetApp.SqlDbServices
+{
+  public class BudgetAppDbContext : DbContext
+  {
+    public BudgetAppDbContext(DbContextOptions<BudgetAppDbContext> options) : base(options) { }
+
+    public DbSet<BudgetCategory> BudgetCategories { get; set; }
+    public DbSet<MonthlyBudget> monthlyBudgets { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<User> Users { get; set; }
+  }
+}
+
