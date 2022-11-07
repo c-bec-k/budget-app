dd7a0737b329a26cfc99682c3a1f5a12f93ca92f
c-bec-k <cruthenbeck@gmail.com>
2022-11-07 11:32:21 -0500

    SQL DB services added

--- BudgetApp.SqlDbServices/BudgetAppDbContext.cs
+++ BudgetApp.SqlDbServices/BudgetAppDbContext.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
-namespace BudgetApp.SqlDbServices
+namespace BudgetApp.SqlDbServices;
+
+public class BudgetAppDbContext : DbContext
 {
-  public class BudgetAppDbContext : DbContext
-  {
-    public BudgetAppDbContext(DbContextOptions<BudgetAppDbContext> options) : base(options) { }
+  public BudgetAppDbContext(DbContextOptions<BudgetAppDbContext> options) : base(options) { }
 
-    public DbSet<BudgetCategory> BudgetCategories { get; set; }
-    public DbSet<MonthlyBudget> monthlyBudgets { get; set; }
-    public DbSet<Transaction> Transactions { get; set; }
-    public DbSet<User> Users { get; set; }
-  }
+  public DbSet<BudgetCategory> BudgetCategories { get; set; }
+  public DbSet<MonthlyBudget> monthlyBudgets { get; set; }
+  public DbSet<Transaction> Transactions { get; set; }
+  public DbSet<User> Users { get; set; }
 }
 
---
+++ BudgetApp.SqlDbServices/SQLUserData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BudgetApp.SqlDbServices;
+
+public class SQLUserData : IUser
+{
+
+  private readonly BudgetAppDbContext _ctx;
+
+  public SQLUserData(BudgetAppDbContext ctx)
+  {
+    var _ctx = ctx;
+  }
+
+  public void Add(User user)
+  {
+    _ctx.Users.Add(user);
+  }
+
+  public User Get(Guid Id)
+  {
+    return _ctx.Users.FirstOrDefault<User>(x => x.Id == Id);
+  }
+
+  public void Update(Guid Id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Delete(Guid Id)
+  {
+    User user = _ctx.Users.FirstOrDefault<User>(x => x.Id == Id);
+    _ctx.Users.Remove(user);
+  }
+
+  public void Commit()
+  {
+    _ctx.SaveChanges();
+  }
+}
+
