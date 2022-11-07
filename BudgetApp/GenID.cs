349f00eaf3176ed9a692f94eca30253c9a816f78
c-bec-k <cruthenbeck@gmail.com>
2022-11-07 11:28:55 -0500

    added ID gen

--- BudgetApp/GenID.cs
+++ BudgetApp/GenID.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using System.Security.Cryptography;
 
-namespace BudgetApp
+namespace BudgetApp;
+
+
+public class GenID
 {
-  public class GenID
+  // GenID should return a ulong that has a
+  // 35-bit timestamp since a specific date (in deciseconds)
+  // 29-bit random number
+
+  private readonly DateTime Epoch;
+  private readonly byte[] rand;
+  private readonly RandomNumberGenerator rng;
+
+  public GenID()
   {
-    // GenID should return a ulong that has a
-    // 35-bit timestamp since a specific date (in deciseconds)
-    // 29-bit random number
+    Epoch = new DateTime(2022, 02, 16);
+    rand = new byte[8];
+    rng = RandomNumberGenerator.Create();
+  }
 
-    private static readonly DateTime Epoch = new DateTime(2022, 02, 16);
-    private readonly byte[] rand = new byte[4];
-    private readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
-
-    public ulong Next()
-    {
-      TimeSpan timestamp = DateTime.UtcNow - Epoch;
-      ulong ts = (ulong)Math.Floor(timestamp.TotalMilliseconds * .01);
-      rng.GetBytes(rand);
-      ulong randoGen = (ulong)BitConverter.ToUInt64(rand);
-      return (ts << 29) | (randoGen & 536870911);
-    }
+  public ulong Next()
+  {
+    TimeSpan timestamp = DateTime.UtcNow - Epoch;
+    ulong ts = (ulong)Math.Floor(timestamp.TotalMilliseconds * .01);
+    Console.WriteLine(ts);
+    rng.GetBytes(rand);
+    ulong randoGen = (ulong)BitConverter.ToUInt64(rand);
+    Console.WriteLine(randoGen);
+    return (ts << 29) | (randoGen & 536870911);
   }
 }
